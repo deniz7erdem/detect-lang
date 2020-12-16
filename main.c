2411ee344c385730e2c1f1cc6612298762713a6a
derdem7 <derdemm7@gmail.com>
2020-12-16 22:26:31 +0300

    Creat repo

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include <ctype.h>
+
+
+const char matrix_bigram_strings[10][3] ={"th", "he", "in", "er", "an", "en", "ch", "de", "ei", "te"};
+
+const char matrix_trigram_strings[10][4]={"the", "and", "ing", "ent", "ion", "der", "sch", "ich", "nde", "die"};
+
+const char languages[2][8]={"english", "german"};
+
+//Ýngilizce dilinin frekans deðerleri
+ const float frequency_eng[20]={ 2.71, 2.33, 2.03, 1.78, 1.61, 1.13, 0.01, 0.01, 0.01, 0.01, 1.81, 0.73, 0.72, 0.42, 0.42, 0.01, 0.01, 0.01, 0.01, 0.01 };
+ //Almanca dilinin frekans deðerleri
+ const float frequency_germ[20]={ 0.01, 0.89, 1.71, 3.90, 1.07, 3.61, 2.36, 2.31, 1.98, 1.98, 0.01, 0.01, 0.01, 0.01, 0.01, 1.04, 0.76, 0.75, 0.72, 0.62 };
+
+
+float calculated_frequencies[20];
+float distances [2]={0,0};
+
+
+void filter_str(char str[]);
+void calculate_frequencies_bi(char str[]);
+void calculate_frequencies_tri(char str[]);
+void calculate_distances();
+void detect_lang();
+
+int main()
+{
+    char metin[50];
+    gets(metin);
+    filter_str(metin);
+    puts(metin);
+
+    return 0;
+}
+
+void filter_str(char str[]){
+    for(int i = 0; i<strlen(str);i++){
+        str[i]= tolower(str[i]);
+        int c = str[i];
+        if((c<97) || (c>122)){
+            str[i]=32;
+        }
+    }
+    str[strlen(str)]="\0";
+}
